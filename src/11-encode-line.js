98a7ef6afb2c5930f5ef9152d46e73f5e0301b70
s1ga <s.v.harlanov@gmail.com>
2021-04-09 19:29:45 +0300

    feat: implement encode-line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let i = 0;
+  let result = '';
+  while (i < arr.length) {
+    let count = 1;
+    if (arr[i + 1]) {
+      while (arr[i] === arr[i + 1]) {
+        count++;
+        i++;
+      }
+    }
+    count = count === 1 ? '' : count;
+    result += `${count}${arr[i]}`;
+    i++;
+  }
+
+  return result;
 }
 
 module.exports = encodeLine;
